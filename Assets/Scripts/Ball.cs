729f3a48e21c0c5ff9de9067518128a8eaa53d19
y_panda_cr <yamada@creatures.jp>
2017-05-30 12:02:12 +0900

    最初のコミット（apkは容量大きいので削除）

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	
+	int speed = 5;
+
+	Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		//右上にボールを動かす
+		rb.AddForce((transform.up + transform.right) * speed, ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
