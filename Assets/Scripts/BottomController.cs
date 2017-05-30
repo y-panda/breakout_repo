be5c2be6c289d11a60694c2cb314c2f1cf8cdcbe
y_panda_cr <yamada@creatures.jp>
2017-05-30 12:14:56 +0900

    BottomScriptを作成

---
+++ Assets/Scripts/BottomController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BottomController : MonoBehaviour {
+
+	public GameObject gameOver;
+	bool goTitle = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (goTitle) {
+			//GameOverの文字が表示された状態で画面をクリック
+			if(Input.GetMouseButtonDown (0)){
+				//Application.LoadLevel ("title");//タイトル画面へ遷移
+				SceneManager.LoadScene("title");
+			}
+
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		Destroy (col.gameObject);
+		//GameOverの文字表示
+		gameOver.SendMessage("Lose");
+		goTitle = true;//update文の実行
+	}
+}
