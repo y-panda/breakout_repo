e00d12f4debee1840efb308270580801d104a3ae
y_panda_cr <yamada@creatures.jp>
2017-05-31 16:39:47 +0900

    ゲームクリア判定を実装。チュートリアル分は完成

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 	
+	public GameObject gameClear;
 	int speed = 5;
-
+	public int blockCt = 20;
 	Rigidbody rb;
+	Vector3 v;
 
-
-	// Use this for initialization
-	void Start () {
+	void Start(){
 		rb = GetComponent<Rigidbody>();
-		//右上にボールを動かす
 		rb.AddForce((transform.up + transform.right) * speed, ForceMode.VelocityChange);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	void Update ()	{
+		//ブロックを全て壊した時
+		if (blockCt == 0) {
+			//ボールの動きを止める
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			//GameClearScriptのWinメソッドを実行しGameClearの文字を表示
+			gameClear.SendMessage ("Win");
+			//クリックしてタイトル画面へ
+			if (Input.GetMouseButtonDown (0)) {
+				Application.LoadLevel ("title");
+			}
+		}
+
+	}
+
+
+
+	void OnCollisionEnter (Collision col){
+		//ブロックにぶつかるとブロックカウント-1
+		if (col.gameObject.tag == "Block") {
+			blockCt -= 1;
+		}
 	}
 }
---
+++ Assets/Scripts/GameClearScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameClearScript : MonoBehaviour {
+
+	void Start () {
+
+		this.gameObject.GetComponent<Text>().enabled = false;
+
+	}
+
+
+
+	public void Win () {
+		gameObject.GetComponent<Text>().enabled = true;
+	}
+}
