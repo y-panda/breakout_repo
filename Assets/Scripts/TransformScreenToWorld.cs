a15e2de22b676ce300f9bcb42b0996e721ed66df
y_panda_cr <yamada@creatures.jp>
2017-06-15 15:54:01 +0900

    目標座標に発射可能にした（仮のオブジェクト）

--- Assets/Scripts/TransformScreenToWorld.cs
+++ Assets/Scripts/TransformScreenToWorld.cs
@@ -9,12 +9,15 @@ public class TransformScreenToWorld : MonoBehaviour {
 	float touchX;
 	float touchY;
 	Vector3 shootVec;
+	Rigidbody rb;
 
-	public GameObject hogeObject;
+	public GameObject orthoObject; //orthographicのカメラから見てる。画面中央(0,0）
+	public GameObject shootObject; //発射する玉
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = shootObject.GetComponent<Rigidbody>();
+		Debug.Log ("orthoObjectから見たshootObject座標"+shootObject.transform.position); 
 	}
 	
 	// Update is called once per frame
@@ -25,11 +28,18 @@ public class TransformScreenToWorld : MonoBehaviour {
 			Camera orthoCamera = gameObject.GetComponent<Camera> ();
 			Vector3 screenPos = Input.mousePosition;
 			//Debug.Log ("screenPos: "+screenPos);
+
 			Vector3 worldPos = orthoCamera.ScreenToWorldPoint(screenPos);
-			Debug.Log ("worldPos: "+worldPos);
-			hogeObject.transform.position = worldPos; 
-			//gameObject.transform.position = worldPos;
+			Debug.Log ("orthoObjectから見た目標座標(worldPos): "+worldPos);
+
+			orthoObject.transform.position = worldPos; //目印移動
 			//Debug.Log ("-----"+gameObject.transform.position);
+			shootVec.x = worldPos.x-shootObject.transform.position.x;
+			shootVec.y = worldPos.y-shootObject.transform.position.y;
+			shootVec.z = worldPos.z;
+			Debug.Log ("shootObjectから見た目標座標: "+shootVec);
+
+			rb.AddForce((shootVec) * (1f), ForceMode.VelocityChange);
 		}
 		//Debug.Log("( "+Input.mousePosition.x+" , "+Input.mousePosition.y+" )");
 
