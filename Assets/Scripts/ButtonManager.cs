e7d3a2e92196b05167cd522de925df6d40a7aeab
y_panda_cr <yamada@creatures.jp>
2017-06-29 19:23:32 +0900

    stage2を作成

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,11 +6,19 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour {
 
 
-	public void OnStage0Load(){
+	public void OnStageLoad(string stageNo){
+		SceneManager.LoadScene("stage"+stageNo);
+	}
+
+	/*public void OnStage0Load(){
 		SceneManager.LoadScene("stage0");
 	}
 
 	public void OnStage1Load(){
 		SceneManager.LoadScene("stage1");
 	}
+
+	public void OnStage2Load(string stageNo){
+		SceneManager.LoadScene("stage2");
+	}*/
 }
